ec2bb1b69271ee0018f22ebd9ee8c4f717b1fabf
Sean Coughlin <sean@froth.com>
2020-02-23 21:09:05 -0500

    Round functions

--- src/gadget.rs
+++ src/gadget.rs
@@ -16,6 +16,61 @@ use pairing::Engine;
 
 use crate::types::{Error, H256, H512};
 
+#[allow(clippy::unreadable_literal)]
+const ROUND_CONSTANTS: [u64; 24] = [
+    0x0000000000000001,
+    0x0000000000008082,
+    0x800000000000808A,
+    0x8000000080008000,
+    0x000000000000808B,
+    0x0000000080000001,
+    0x8000000080008081,
+    0x8000000000008009,
+    0x000000000000008A,
+    0x0000000000000088,
+    0x0000000080008009,
+    0x000000008000000A,
+    0x000000008000808B,
+    0x800000000000008B,
+    0x8000000000008089,
+    0x8000000000008003,
+    0x8000000000008002,
+    0x8000000000000080,
+    0x000000000000800A,
+    0x800000008000000A,
+    0x8000000080008081,
+    0x8000000000008080,
+    0x0000000080000001,
+    0x8000000080008008,
+];
+const ROTR: [usize; 25] = [ //(5*y)+x
+    0,
+    1,
+    62,
+    28,
+    27,
+    36,
+    44,
+    6,
+    55,
+    20,
+    3,
+    10,
+    43,
+    25,
+    39,
+    41,
+    45,
+    15,
+    21,
+    8,
+    18,
+    2,
+    61,
+    56,
+    14,
+];
+
 #[derive(Clone)]
 struct Sha3_256GadgetInput {
     /// Hash
@@ -88,6 +143,87 @@ impl AsRef<[u8]> for Proof {
     }
 }
 
+//Process
+fn keccak_f_1600(input: Vec<bool>) -> Vec<bool> {
+    let mut A = input;
+
+    for i in 0..24 {
+        A = Round_1600(&A, ROUND_CONSTANTS[i]);
+    }
+
+    return A;
+}
+
+fn Round_1600(A: &Vec<bool>, RC: u64) -> Vec<bool> {
+    assert_eq!(A.len(), 1600);//64*25
+
+    // # θ step
+    // C[x] = A[x,0] xor A[x,1] xor A[x,2] xor A[x,3] xor A[x,4],   for x in 0…4
+    let mut C = vec![false; 320];
+    for x in 0..5 {
+        for bit in 0..64 {
+            C[(x * 64usize) + bit] = A[(x * 64usize) + bit + (0usize * 64usize)] ^ A[(x * 64usize) + bit + (1usize * 64usize)] ^ A[(x * 64usize) + bit + (2usize * 64usize)] ^ A[(x * 64usize) + bit + (3usize * 64usize)] ^ A[(x * 64usize) + bit + (4usize * 64usize)];
+        }
+    }
+    // D[x] = C[x-1] xor rot(C[x+1],1),                             for x in 0…4
+    let mut D = vec![false; 320];
+    for x in 0..5 {
+        for bit in 0..64 {
+            //x-1 == (x+4)%5
+            D[(x * 64usize) + bit] = C[(((x + 4usize) % 5usize) * 64usize) + bit] ^ C[(((x + 1usize) % 5usize) * 64usize) + ((bit - 1usize) % 64)];
+        }
+    }
+    // A[x,y] = A[x,y] xor D[x],                           for (x,y) in (0…4,0…4)
+    let mut A_new = vec![false; 1600];
+    for x in 0..5 {
+        for y in 0..5 {
+            for bit in 0..64 {
+                A_new[(y * 320usize) + (x * 64usize) + bit] = A[(y * 320usize) + (x * 64usize) + bit] ^ D[(x * 64usize) + bit];
+            }
+        }
+    }
+
+    // # ρ and π steps
+    // B[y,2*x+3*y] = rot(A[x,y], r[x,y]),                 for (x,y) in (0…4,0…4)
+    let mut B = vec![false; 1600];
+    for x in 0..5 {
+        for y in 0..5 {
+            for bit in 0..64 {
+                B[((((2usize * x) + (3usize * y)) % 5) * 320usize) + (y * 64usize) + bit] = A_new[(y * 320usize) + (x * 64usize) + ((ROTR[(y * 5usize) + x] + bit) % 64usize)];
+            }
+        }
+    }
+
+    // # χ step
+    // A[x,y] = B[x,y] xor ((not B[x+1,y]) and B[x+2,y]),  for (x,y) in (0…4,0…4)
+    let mut A_new2 = vec![false; 1600];
+    for x in 0..5 {
+        for y in 0..5 {
+            for bit in 0..64 {
+                A_new2[(y * 320usize) + (x * 64usize) + bit] = B[(y * 320usize) + (x * 64usize) + bit] ^ (!B[(y * 320usize) + (((x + 1usize) % 5usize) * 64usize) + bit] & B[(y * 320usize) + (((x + 2usize) % 5usize) * 64usize) + bit]);
+            }
+        }
+    }
+
+    // # ι step
+    let mut RC_bits = vec![false; 64];
+    for bit in 0..64 {
+        let val = 1u64 << bit;
+        if RC & val != 0u64 {
+            RC_bits[bit] = true;
+        }
+    }
+
+    // A[0,0] = A[0,0] xor RC
+    for bit in 0..64 {
+        A_new2[bit] = A_new2[bit] ^ RC_bits[bit];
+    }
+
+    return A_new2;
+}
+
+
+//Circuit & gadget
 impl<E: Engine> Circuit<E> for Sha3_256Gadget {
     fn synthesize<CS: ConstraintSystem<E>>(self, mut cs: &mut CS) -> Result<(), SynthesisError> {
         //TODO: Implement
@@ -95,6 +231,7 @@ impl<E: Engine> Circuit<E> for Sha3_256Gadget {
     }
 }
 
+//Test
 #[cfg(test)]
 mod test {
     use crate::types::H256; //Error, Secp256k1Point
