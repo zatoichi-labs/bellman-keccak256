ca4c5f97a057797d3c7a00e6d468791b4ef9922d
Bryant Eisenbach <fubuloubu@gmail.com>
2020-03-09 14:08:01 -0400

    refactor: Remove unnecessary Python and CLI stuff

--- src/lib.rs
+++ src/lib.rs
@@ -2,23 +2,15 @@
 extern crate serde_derive;
 
 mod api;
-#[cfg(all(feature = "std", not(feature = "python")))]
+#[cfg(not(feature = "std"))]
 pub use api::generate;
-#[cfg(all(not(feature = "std"), not(feature = "python")))]
+#[cfg(not(feature = "std"))]
 pub use api::Seed;
-#[cfg(not(feature = "python"))]
 pub use api::{prove, verify};
 
 mod gadget;
 mod uint64;
-#[cfg(not(feature = "python"))]
 pub use gadget::{Proof, SetupParams};
 
 mod types;
-#[cfg(not(feature = "python"))]
 pub use types::{Error, Login, H128, H256};
-
-#[cfg(feature = "python")]
-mod python;
-#[cfg(feature = "python")]
-pub use python::keccak;
