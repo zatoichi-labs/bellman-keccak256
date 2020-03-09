cda49e386e0c622638f2c4f567c8f0fa8690f0ec
Bryant Eisenbach <fubuloubu@gmail.com>
2020-03-09 17:09:59 -0400

    refactor: Remove API module

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,6 @@
 #[macro_use]
 extern crate serde_derive;
 
-mod api;
-#[cfg(not(feature = "std"))]
-pub use api::generate;
-#[cfg(not(feature = "std"))]
-pub use api::Seed;
-pub use api::{prove, verify};
-
 mod gadget;
 mod uint64;
 pub use gadget::{Proof, SetupParams};
